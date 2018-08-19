c5aa797e0348a9877101e9054fc37a18aaa2258a
angalet <41893557+angalet@users.noreply.github.com>
2018-08-19 20:08:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -59,8 +59,6 @@ $animals = array(
             $animal[]  = $value;
             $part_animal = explode(" ",$value);
             echo "Континент - $array, животное # - $value <br>";
-            //echo $part_animal[0]."<br>";
-            //echo $part_animal[1]."<br>";
             if (!$part_animal[1] == NULL)
             {
             $part_animal_first[] = $part_animal[0];
@@ -69,34 +67,17 @@ $animals = array(
         }
     }
     ?></div><?php
-    //$new_animals_ = array();
-    //$new_animals_ = array_merge($animals["africa"], $animals["noth_america"]);
 echo "<pre>";
-//echo "Начальный <br>";
-//print_r($animals);
-//echo "Итоговый массив <br>";
-//print_r($animal);
-//print_r($part_animal_first);
-//sort($part_animal_first);
 shuffle($part_animal_first);
 shuffle($part_animal_second);
-//echo $part_animal_first[0] . "\n";
 $wonderful_animal = array();
 for ($i = count($part_animal_first); $i > 0; $i--){
 $rand = rand(0,$i);
-//echo $rand."<br>";
 $a = array_pop($part_animal_first);
 $b = array_pop($part_animal_second);
 $c = $a." ".$b;
-//echo $c."<br>";
 $wonderful_animal[] = $c;
 }
-
-//print_r($part_animal_first);
-//print_r($part_animal_second);
-
-//print_r($wonderful_animal);
-//print_r($new_animals_);
 echo "</pre>";
 echo "<h2>Перемешанный массив </h2><br>";
 ?><div><?php
